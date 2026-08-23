407bcf5456cfa69fada3deda0b7adaccb5f3422a
Marco <marcofratini@outlook.com>
2026-08-23 11:22:03 +0200

    New ammo registries

---
+++ Contents/mods/Gunworks_gang_framework/42.13/media/registries.lua
@@ -0,0 +1,46 @@
+SWMG_AmmoTypes = {}
+
+SWMG_AmmoTypes.BULLET_9x19 = AmmoType.register("swmg:bullet_9x19", "SWMG.9x19_Bullet")
+
+SWMG_AmmoTypes.BULLET_45 = AmmoType.register("swmg:bullet_45", "SWMG.45_Bullet")
+
+SWMG_AmmoTypes.BULLET_38 = AmmoType.register("swmg:bullet_38", "SWMG.38_Bullet")
+
+SWMG_AmmoTypes.BULLET_357 = AmmoType.register("swmg:bullet_357", "SWMG.357_Bullet")
+
+SWMG_AmmoTypes.BULLET_44 = AmmoType.register("swmg:bullet_44", "SWMG.44_Bullet")
+
+SWMG_AmmoTypes.BULLET_50 = AmmoType.register("swmg:bullet_50", "SWMG.50_Bullet")
+
+SWMG_AmmoTypes.BULLET_57x28 = AmmoType.register("swmg:bullet_57x28", "SWMG.57x28_Bullet")
+
+SWMG_AmmoTypes.BULLET_223 = AmmoType.register("swmg:bullet_223", "SWMG.223_Bullet")
+SWMG_AmmoTypes.BULLET_556x45 = AmmoType.register("swmg:bullet_556x45", "SWMG.556x45_Bullet")
+SWMG_AmmoTypes.BULLET_556x45_Subsonic = AmmoType.register("swmg:bullet_556x45_subsonic", "SWMG.556x45_Bullet_Subsonic")
+SWMG_AmmoTypes.BULLET_556x45_ArmorPiercing = AmmoType.register("swmg:bullet_556x45_armor_piercing", "SWMG.556x45_Bullet_ArmorPiercing")
+SWMG_AmmoTypes.BULLET_556x45_HollowPoint = AmmoType.register("swmg:bullet_556x45_hollow_point", "SWMG.556x45_Bullet_HollowPoint")
+SWMG_AmmoTypes.BULLET_556x45_Overpressured = AmmoType.register("swmg:bullet_556x45_overpressured", "SWMG.556x45_Bullet_Overpressured")
+
+SWMG_AmmoTypes.BULLET_545x39 = AmmoType.register("swmg:bullet_545x39", "SWMG.545x39_Bullet")
+
+SWMG_AmmoTypes.BULLET_762x39 = AmmoType.register("swmg:bullet_762x39", "SWMG.762x39_Bullet")
+
+SWMG_AmmoTypes.BULLET_3030 = AmmoType.register("swmg:bullet_3030", "SWMG.3030_Bullet")
+
+SWMG_AmmoTypes.BULLET_308 = AmmoType.register("swmg:bullet_308", "SWMG.308_Bullet")
+SWMG_AmmoTypes.BULLET_762x51 = AmmoType.register("swmg:bullet_762x51", "SWMG.762x51_Bullet")
+
+SWMG_AmmoTypes.BULLET_4570 = AmmoType.register("swmg:bullet_4570", "SWMG.4570_Bullet")
+
+SWMG_AmmoTypes.BULLET_762x54 = AmmoType.register("swmg:bullet_762x54", "SWMG.762x54_Bullet")
+
+SWMG_AmmoTypes.BULLET_9x39 = AmmoType.register("swmg:bullet_9x39", "SWMG.9x39_Bullet")
+
+SWMG_AmmoTypes.SHELL_12G_BUCKSHOT = AmmoType.register("swmg:shell_12g_buckshot", "SWMG.12Gauge_Shell_Buckshot")
+SWMG_AmmoTypes.SHELL_12G_SLUG = AmmoType.register("swmg:shell_12g_slug", "SWMG.12Gauge_Shell_Slug")
+
+SWMG_AmmoTypes.BULLET_3006 = AmmoType.register("swmg:bullet_3006", "SWMG.3006_Bullet")
+
+SWMG_AmmoTypes.ROUND_40MM_BUCKSHOT = AmmoType.register("swmg:round_40mm_buckshot", "SWMG.40mm_Round_Buckshot")
+SWMG_AmmoTypes.ROUND_40MM_HE = AmmoType.register("swmg:round_40mm_he", "SWMG.40mm_Round_HE")
+SWMG_AmmoTypes.ROUND_40MM_INCENDIARY = AmmoType.register("swmg:round_40mm_incendiary", "SWMG.40mm_Round_Incendiary")
